5debea9da008eb7f2b1e7a9f2864293f02da31b7
CheesyChocolate <dev@behnamlal.xyz>
2024-03-10 13:07:59 +0000

    add(challenge): transpile the source to python

---
+++ misc_colored_squares/src.py
@@ -0,0 +1,129 @@
+print("Enter the flag in decimal (one character per line) :\n", end='', flush=True)
+var_0 = input()
+if var_0.isdigit():
+    var_0 = int(var_0)
+else:
+    var_0 = var_0
+var_1 = input()
+if var_1.isdigit():
+    var_1 = int(var_1)
+else:
+    var_1 = var_1
+var_2 = input()
+if var_2.isdigit():
+    var_2 = int(var_2)
+else:
+    var_2 = var_2
+var_3 = input()
+if var_3.isdigit():
+    var_3 = int(var_3)
+else:
+    var_3 = var_3
+var_4 = input()
+if var_4.isdigit():
+    var_4 = int(var_4)
+else:
+    var_4 = var_4
+var_5 = input()
+if var_5.isdigit():
+    var_5 = int(var_5)
+else:
+    var_5 = var_5
+var_6 = input()
+if var_6.isdigit():
+    var_6 = int(var_6)
+else:
+    var_6 = var_6
+var_7 = input()
+if var_7.isdigit():
+    var_7 = int(var_7)
+else:
+    var_7 = var_7
+var_8 = input()
+if var_8.isdigit():
+    var_8 = int(var_8)
+else:
+    var_8 = var_8
+var_9 = input()
+if var_9.isdigit():
+    var_9 = int(var_9)
+else:
+    var_9 = var_9
+var_10 = input()
+if var_10.isdigit():
+    var_10 = int(var_10)
+else:
+    var_10 = var_10
+var_11 = input()
+if var_11.isdigit():
+    var_11 = int(var_11)
+else:
+    var_11 = var_11
+var_12 = input()
+if var_12.isdigit():
+    var_12 = int(var_12)
+else:
+    var_12 = var_12
+var_13 = input()
+if var_13.isdigit():
+    var_13 = int(var_13)
+else:
+    var_13 = var_13
+var_14 = input()
+if var_14.isdigit():
+    var_14 = int(var_14)
+else:
+    var_14 = var_14
+var_15 = input()
+if var_15.isdigit():
+    var_15 = int(var_15)
+else:
+    var_15 = var_15
+var_16 = input()
+if var_16.isdigit():
+    var_16 = int(var_16)
+else:
+    var_16 = var_16
+var_17 = input()
+if var_17.isdigit():
+    var_17 = int(var_17)
+else:
+    var_17 = var_17
+var_18 = input()
+if var_18.isdigit():
+    var_18 = int(var_18)
+else:
+    var_18 = var_18
+var_19 = input()
+if var_19.isdigit():
+    var_19 = int(var_19)
+else:
+    var_19 = var_19
+var_20 = input()
+if var_20.isdigit():
+    var_20 = int(var_20)
+else:
+    var_20 = var_20
+var_21 = input()
+if var_21.isdigit():
+    var_21 = int(var_21)
+else:
+    var_21 = var_21
+if (((var_7) - (var_18)) == ((var_8) - (var_9))):
+    if (((var_6) + (var_10)) == (((var_16) + (var_20)) + (12))):
+        if (((var_8) * (var_14)) == (((var_13) * (var_18)) * (2))):
+            if ((var_19) == (var_6)):
+                if (((var_9) + (1)) == ((var_17) - (1))):
+                    if (((var_11) / ((var_5) + (7))) == (2)):
+                        if (((var_5) + ((var_2) / (2))) == (var_1)):
+                            if (((var_16) - (9)) == ((var_13) + (4))):
+                                if (((var_12) / (3)) == (17)):
+                                    if ((((var_4) - (var_5)) + (var_12)) == ((var_14) + (20))):
+                                        if ((((var_12) * (var_15)) / (var_14)) == (24)):
+                                            if ((var_18) == ((173) - (var_4))):
+                                                if ((var_6) == ((63) + (var_5))):
+                                                    if (((32) * (var_16)) == ((var_7) * (var_0))):
+                                                        if ((125) == (var_21)):
+                                                            if (((var_3) - (var_2)) == (57)):
+                                                                if (((var_17) - (var_15)) == ((var_18) + (1))):
+                                                                    print("Good job! :)", end='', flush=True)
